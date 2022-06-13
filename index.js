aaccbfeb0b59e8169ea9b0c0de2ab4835e2018f9
Bahoz <bahozali50@gmail.com>
2022-06-13 18:13:56 +0300

    add closeMobile menu and give it to all navItems.

--- index.js
+++ index.js
@@ -1,22 +1,26 @@
 const menuButton = document.getElementById("menu");
 const nav_list = document.getElementsByClassName("nav-list")[0];
 const xButton = document.getElementsByClassName("bi-x-lg")[0];
+const navItems = document.getElementsByClassName("nav__item");
 const body = document.body;
 
 menuButton.addEventListener("click", (event) => {
-  nav_list.classList.toggle("nav--open");
-  menuButton.style.display = "none";
-
-  body.style.overflow = 'hidden';
-  xButton.classList.toggle("xopen");
-
-
-  // if (xButton.style.display == "none") {
-  //   xButton.style.display = "block";
-  //   xButton.style.fontSize = "400px";
-  // } else {
-  //   xButton.style.display = "none";
-
-  // }
-
+  nav_list.classList.add("nav--open");
+  menuButton.classList.add("menu--close");
+  body.style.overflow = "hidden";
+  xButton.classList.add("xopen");
 });
+
+xButton.addEventListener("click", closeMobileMenu);
+
+for (const item of navItems) {
+  item.addEventListener("click", closeMobileMenu)
+}
+
+
+function closeMobileMenu() {
+   nav_list.classList.remove("nav--open");
+  menuButton.classList.remove("menu--close");
+  xButton.classList.remove("xopen");
+  body.style.overflowY = "scroll";
+}
