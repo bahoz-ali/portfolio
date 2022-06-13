144f73f4e518feae021cdf30e91f8c17894552b6
Bahoz Ali <bahozali50@gmail.com>
2022-06-13 15:54:39 +0300

    display navlist in movile screen by using js

---
+++ index.js
@@ -0,0 +1,12 @@
+const menuButton = document.getElementById("menu");
+const nav_list = document.getElementsByClassName("nav-list")[0];
+const xButton = document.getElementsByClassName("bi-x-lg")[0];
+
+menuButton.addEventListener("click", (event) => {
+  nav_list.classList.toggle("nav--open");
+
+  menuButton.style.display = "none";
+
+  if ((xButton.style.display = "none")) xButton.style.display = "block";
+  else xButton.style.display = "none";
+});
