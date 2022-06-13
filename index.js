b0d6d787b4a2ad2f8907906115f996dfc380459b
gak92 <44802922+gak92@users.noreply.github.com>
2022-06-13 19:15:38 +0500

    Add stop scrolling features while displaying menu

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const menuButton = document.getElementById("menu");
 const nav_list = document.getElementsByClassName("nav-list")[0];
 const xButton = document.getElementsByClassName("bi-x-lg")[0];
+const body = document.body;
 
 menuButton.addEventListener("click", (event) => {
   nav_list.classList.toggle("nav--open");
-  // nav_list.style.display = "block"
   menuButton.style.display = "none";
+
+  body.style.overflow = 'hidden';
 
   if (xButton.style.display == "none") {
     xButton.style.display = "block";
