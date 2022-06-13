1c1541fb74792cfbac3aeead50c89f3a78070fd0
Bahoz Ali <bahozali50@gmail.com>
2022-06-13 17:03:55 +0300

    giving css to the navlist for mobile versioin

--- index.js
+++ index.js
@@ -4,9 +4,14 @@ const xButton = document.getElementsByClassName("bi-x-lg")[0];
 
 menuButton.addEventListener("click", (event) => {
   nav_list.classList.toggle("nav--open");
-
+  // nav_list.style.display = "block"
   menuButton.style.display = "none";
 
-  if ((xButton.style.display = "none")) xButton.style.display = "block";
-  else xButton.style.display = "none";
+  if (xButton.style.display == "none") {
+    xButton.style.display = "block";
+  } else {
+    xButton.style.display = "none";
+
+  }
+
 });
