c31044cfe8e416c784f60d8170056e28074428ad
Bahoz <bahozali50@gmail.com>
2022-06-14 10:17:36 +0300

    fixing the linter errors

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
-const menuButton = document.getElementById("menu");
-const nav_list = document.getElementsByClassName("nav-list")[0];
-const xButton = document.getElementsByClassName("bi-x-lg")[0];
-const navItems = document.getElementsByClassName("nav__item");
-const body = document.body;
-
-menuButton.addEventListener("click", (event) => {
-  nav_list.classList.add("nav--open");
-  menuButton.classList.add("menu--close");
-  body.style.overflow = "hidden";
-  xButton.classList.add("xopen");
-});
-
-xButton.addEventListener("click", closeMobileMenu);
-
-for (const item of navItems) {
-  item.addEventListener("click", closeMobileMenu);
-}
+const menuButton = document.getElementById('menu');
+const navList = document.getElementsByClassName('nav-list')[0];
+const xButton = document.getElementsByClassName('bi-x-lg')[0];
+const navItems = document.getElementsByClassName('nav__item');
+const { body } = document;
 
 function closeMobileMenu() {
-  nav_list.classList.remove("nav--open");
-  menuButton.classList.remove("menu--close");
-  xButton.classList.remove("xopen");
-  body.style.overflowY = "scroll";
+  navList.classList.remove('nav--open');
+  menuButton.classList.remove('menu--close');
+  xButton.classList.remove('xopen');
+  body.style.overflowY = 'scroll';
 }
+
+menuButton.addEventListener('click', () => {
+  navList.classList.add('nav--open');
+  menuButton.classList.add('menu--close');
+  body.style.overflow = 'hidden';
+  xButton.classList.add('xopen');
+});
+
+xButton.addEventListener('click', closeMobileMenu);
+
+Array.from(navItems).forEach((item) => {
+  item.addEventListener('click', closeMobileMenu);
+});
